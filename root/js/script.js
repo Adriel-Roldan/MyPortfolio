978856ad9e87ff0c52338b030b0b448a205555d9
Adriel-Roldan <adrielemilioroldanchitadino@gmail.com>
2022-09-05 11:23:44 -0300

    Diseño mobile terminado

---
+++ root/js/script.js
@@ -0,0 +1,16 @@
+document.querySelector(".bars__menu").addEventListener("click", animateBars);
+
+
+let line1__bars = document.querySelector(".line1__bars-menu");
+
+let line2__bars = document.querySelector(".line2__bars-menu");
+
+let line3__bars = document.querySelector(".line3__bars-menu");
+
+function animateBars(){
+    line1__bars.classList.toggle("activeline1__bars-menu");
+
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    
+    line3__bars.classList.toggle("activeline3__bars-menu");
+}
